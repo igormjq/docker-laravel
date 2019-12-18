164b54f35a01bd5ac697120a0f0569aaad33272d
Igor Marini Jaekel Quevedo <igormjqatlas@gmail.com>
2019-12-18 13:25:32 -0300

    :pencil2: Change text to test CI build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Code Education Laravel
                 </div>
 
                 <div class="links">
